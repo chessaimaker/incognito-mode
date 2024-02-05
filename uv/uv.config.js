77441dfeea0ab9c9e2eddf561c321325e10c5d3c
chessaimaker <147644008+chessaimaker@users.noreply.github.com>
2024-02-05 05:10:56 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
+    bare: 'https://www.local-jamatkhana-naandhi-shopping.islam.cl/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
